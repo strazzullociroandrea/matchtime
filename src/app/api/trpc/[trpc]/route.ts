4f69eb6ceeac3d42b657ab8b425b62e8754b51fc
ciro <ciroandreastrazzullo@icloud.com>
2026-02-11 15:08:17 +0100

    feat: add initial layout and API setup for match time tracker

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import type { NextRequest } from "next/server";
+import { appRouter } from "@/server/api/root";
+import { createTRPCContext } from "@/server/api/trpc";
+
+const handler = async (req: NextRequest) =>
+    fetchRequestHandler({
+        endpoint: "/api/trpc",
+        req,
+        router: appRouter,
+        createContext: async () => await createTRPCContext(req),
+    });
+
+export { handler as GET, handler as POST };
