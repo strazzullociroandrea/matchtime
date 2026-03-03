658adc7360dddfd3fe76d4e7daa9255239339a51
ciro <ciroandreastrazzullo@icloud.com>
2026-03-03 20:44:30 +0100

    Fix: fix notification

--- src/server/api/routers/api/sendWeeklyReminder.ts
+++ src/server/api/routers/api/sendWeeklyReminder.ts
@@ -127,7 +127,7 @@ export const sendWeeklyReminder = async ({
 
       for (let i = 7; i >= 1; i--) {
         const today = new Date();
-        if(daysUntil(matchDate) > i) continue;
+        if(today > matchDate) continue;
         const notificationDate = new Date(matchDate);
         notificationDate.setDate(notificationDate.getDate() - i);
         notificationDate.setHours(hours, minutes || 19, 15, 0);
