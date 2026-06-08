43063f6ee186543b5cc253ed3c5e3e8b8de39327
ciro <ciroandreastrazzullo@icloud.com>
2026-06-08 19:54:36 +0200

    feat: update Cloudflare adapter mode to directory and add deploy script in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
-      mode: 'pages'
+      mode: 'directory'
     }),
   output: 'server',
   vite: {
